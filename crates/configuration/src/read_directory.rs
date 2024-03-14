029ff0f95d4724a4fed5b6ecb551baa0d2368e06
Jesse Hallett <jesse.hallett@hasura.io>
2024-03-14 13:38:54 -0700

    read schema and native queries from separate files

--- crates/configuration/src/read_directory.rs
+++ crates/configuration/src/read_directory.rs
@@ -1,3 +1,4 @@
+use futures::stream::TryStreamExt as _;
 use itertools::Itertools as _;
 use serde::Deserialize;
 use std::{
@@ -5,10 +6,13 @@ use std::{
     path::{Path, PathBuf},
 };
 use tokio::fs;
+use tokio_stream::wrappers::ReadDirStream;
 
-use crate::Configuration;
+use crate::{native_queries::NativeQuery, Configuration};
 
-pub const CONFIGURATION_FILENAME: &str = "configuration";
+pub const METADATA_FILENAME: &str = "metadata";
+pub const NATIVE_QUERIES_DIRNAME: &str = "native_queries";
+
 pub const CONFIGURATION_EXTENSIONS: [(&str, FileFormat); 3] =
     [("json", JSON), ("yaml", YAML), ("yml", YAML)];
 
@@ -25,29 +29,68 @@ const YAML: FileFormat = FileFormat::Yaml;
 pub async fn read_directory(
     configuration_dir: impl AsRef<Path> + Send,
 ) -> io::Result<Configuration> {
-    parse_file(configuration_dir, CONFIGURATION_FILENAME).await
+    let dir = configuration_dir.as_ref();
+
+    let metadata = parse_json_or_yaml(dir, METADATA_FILENAME).await?;
+
+    let native_queries: Vec<NativeQuery> =
+        read_subdir_configs(&dir.join(NATIVE_QUERIES_DIRNAME)).await?;
+
+    Ok(Configuration {
+        metadata,
+        native_queries,
+    })
+}
+
+/// Parse all files in a directory with one of the allowed configuration extensions according to
+/// the given type argument. For example if `T` is `NativeQuery` this function assumes that all
+/// json and yaml files in the given directory should be parsed as native query configurations.
+async fn read_subdir_configs<T>(subdir: &Path) -> io::Result<Vec<T>>
+where
+    for<'a> T: Deserialize<'a>,
+{
+    let dir_stream = ReadDirStream::new(fs::read_dir(subdir).await?);
+    dir_stream
+        .try_filter_map(|dir_entry| async move {
+            // Permits regular files and symlinks, does not filter out symlinks to directories.
+            let is_file = !(dir_entry.file_type().await?.is_dir());
+            if !is_file {
+                return Ok(None);
+            }
+
+            let path = dir_entry.path();
+            let extension = path.extension().and_then(|ext| ext.to_str());
+
+            let format_option = extension
+                .and_then(|ext| {
+                    CONFIGURATION_EXTENSIONS
+                        .iter()
+                        .find(|(expected_ext, _)| ext == *expected_ext)
+                })
+                .map(|(_, format)| *format);
+
+            Ok(format_option.map(|format| (path, format)))
+        })
+        .and_then(|(path, format)| async move { parse_config_file::<T>(path, format).await })
+        .try_collect::<Vec<T>>()
+        .await
 }
 
 /// Given a base name, like "connection", looks for files of the form "connection.json",
 /// "connection.yaml", etc; reads the file; and parses it according to its extension.
-async fn parse_file<T>(configuration_dir: impl AsRef<Path>, basename: &str) -> io::Result<T>
+async fn parse_json_or_yaml<T>(configuration_dir: &Path, basename: &str) -> io::Result<T>
 where
     for<'a> T: Deserialize<'a>,
 {
     let (path, format) = find_file(configuration_dir, basename).await?;
-    read_file(path, format).await
+    parse_config_file(path, format).await
 }
 
 /// Given a base name, like "connection", looks for files of the form "connection.json",
 /// "connection.yaml", etc, and returns the found path with its file format.
-async fn find_file(
-    configuration_dir: impl AsRef<Path>,
-    basename: &str,
-) -> io::Result<(PathBuf, FileFormat)> {
-    let dir = configuration_dir.as_ref();
-
+async fn find_file(configuration_dir: &Path, basename: &str) -> io::Result<(PathBuf, FileFormat)> {
     for (extension, format) in CONFIGURATION_EXTENSIONS {
-        let path = dir.join(format!("{basename}.{extension}"));
+        let path = configuration_dir.join(format!("{basename}.{extension}"));
         if fs::try_exists(&path).await? {
             return Ok((path, format));
         }
@@ -57,7 +100,7 @@ async fn find_file(
         io::ErrorKind::NotFound,
         format!(
             "could not find file, {:?}",
-            dir.join(format!(
+            configuration_dir.join(format!(
                 "{basename}.{{{}}}",
                 CONFIGURATION_EXTENSIONS
                     .into_iter()
@@ -68,7 +111,7 @@ async fn find_file(
     ))
 }
 
-async fn read_file<T>(path: impl AsRef<Path>, format: FileFormat) -> io::Result<T>
+async fn parse_config_file<T>(path: impl AsRef<Path>, format: FileFormat) -> io::Result<T>
 where
     for<'a> T: Deserialize<'a>,
 {
