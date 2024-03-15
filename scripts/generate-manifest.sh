652232660d96c922bbd6b99720b267436026f619
Daniel Chambers <daniel@hasura.io>
2024-03-15 14:58:05 +1100

    Rename everything

--- scripts/generate-manifest.sh
+++ scripts/generate-manifest.sh
@@ -3,11 +3,11 @@
 set -evo pipefail
 ROOT="$(pwd)"
 
-export LINUX_AMD64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep hasura-mongodb-x86_64-unknown-linux-gnu   | cut -f1 -d' ')
-export MACOS_AMD64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep hasura-mongodb-x86_64-apple-darwin        | cut -f1 -d' ')
-export WINDOWS_AMD64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum | grep hasura-mongodb-x86_64-pc-windows-msvc.exe | cut -f1 -d' ')
-export LINUX_ARM64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep hasura-mongodb-aarch64-unknown-linux-gnu  | cut -f1 -d' ')
-export MACOS_ARM64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep hasura-mongodb-aarch64-apple-darwin       | cut -f1 -d' ')
+export LINUX_AMD64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep mongodb-cli-plugin-x86_64-unknown-linux-gnu   | cut -f1 -d' ')
+export MACOS_AMD64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep mongodb-cli-plugin-x86_64-apple-darwin        | cut -f1 -d' ')
+export WINDOWS_AMD64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum | grep mongodb-cli-plugin-x86_64-pc-windows-msvc.exe | cut -f1 -d' ')
+export LINUX_ARM64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep mongodb-cli-plugin-aarch64-unknown-linux-gnu  | cut -f1 -d' ')
+export MACOS_ARM64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep mongodb-cli-plugin-aarch64-apple-darwin       | cut -f1 -d' ')
 
 (echo "cat <<EOF >${ROOT}/release/manifest.yaml";
 cat ${ROOT}/scripts/manifest.yaml; echo; echo EOF;
