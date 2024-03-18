1a1f0388545d0acf0ea25dbb7b948900ddca0ca3
David Overton <david@hasura.io>
2024-03-18 11:49:04 +1100

    Rename Metadata to Schema

--- crates/cli/src/introspection/document.rs
+++ crates/cli/src/introspection/document.rs
@@ -1,14 +1,14 @@
 use configuration::{
-    metadata::{Collection, ObjectField, ObjectType, Type},
-    Metadata,
+    schema::{Collection, ObjectField, ObjectType, Type},
+    Schema,
 };
 use mongodb::bson::{Bson, Document};
 use mongodb_agent_common::interface_types::{MongoAgentError, MongoConfig};
 use mongodb_support::{BsonScalarType, BsonType};
 
-pub fn schema_from_document(collection_name: &str, document: &Document) -> Metadata {
+pub fn schema_from_document(collection_name: &str, document: &Document) -> Schema {
   let (object_types, collection) = make_collection(collection_name, document);
-  Metadata { collections: vec!(collection), object_types}
+  Schema { collections: vec!(collection), object_types}
 }
 
 fn make_collection(collection_name: &str, document: &Document) -> (Vec<ObjectType>, Collection) {
