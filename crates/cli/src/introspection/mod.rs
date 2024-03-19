ff6b69291db15d4cec23976c14cf4111a6d9aa9f
David Overton <david@hasura.io>
2024-03-19 18:17:41 +1100

    Rename document.rs to sampling.rs

--- crates/cli/src/introspection/mod.rs
+++ crates/cli/src/introspection/mod.rs
@@ -1,6 +1,6 @@
-pub mod document;
+pub mod sampling;
 pub mod validation_schema;
 pub mod type_unification;
 
 pub use validation_schema::get_metadata_from_validation_schema;
-pub use document::sample_schema_from_db;
+pub use sampling::sample_schema_from_db;
