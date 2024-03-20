9d2485cb94d489abedb846a55cdb6bfb8f390762
Jesse Hallett <jesse.hallett@hasura.io>
2024-03-19 18:31:43 -0700

    fix: insert nulls in place of missing database document fields

--- crates/mongodb-agent-common/src/query/mod.rs
+++ crates/mongodb-agent-common/src/query/mod.rs
@@ -77,7 +77,7 @@ mod tests {
 
         let expected_pipeline = json!([
             { "$match": { "gpa": { "$lt": 4.0 } } },
-            { "$replaceWith": { "student_gpa": "$gpa" } },
+            { "$replaceWith": { "student_gpa": { "$ifNull": ["$gpa", null] } } },
         ]);
 
         let mut collection = MockCollectionTrait::new();
@@ -222,7 +222,7 @@ mod tests {
                     ],
                     "__ROWS__": [{
                         "$replaceWith": {
-                            "student_gpa": "$gpa",
+                            "student_gpa": { "$ifNull": ["$gpa", null] },
                         },
                     }],
                 },
@@ -300,7 +300,7 @@ mod tests {
                 "$replaceWith": {
                     "date": {
                         "$dateToString": {
-                            "date": "$date",
+                            "date": { "$ifNull": ["$date", null] },
                         },
                     },
                 }
