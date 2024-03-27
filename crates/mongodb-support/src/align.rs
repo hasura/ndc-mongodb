f29fa079d8824d4606ebc401d8487e97e43da0a2
David Overton <dmoverton@users.noreply.github.com>
2024-03-28 09:27:31 +1100

    Add `any` type (#18)
    * Introduce Any type
    
    * Avoid renormalizing recursive types
    
    * Add ScalarTypeCapabilities for the "any" type
    
    * Add changelog
    
    * Add some documentation on unify_type
    
    * A few minor fixes
    
    * Handle Type::Any when converting from JSON to BSON

--- crates/mongodb-support/src/align.rs
+++ crates/mongodb-support/src/align.rs
@@ -1,24 +1,24 @@
 use indexmap::IndexMap;
 use std::hash::Hash;
 
-pub fn align_with_result<K, T, U, V, E, FT, FU, FTU>(ts: IndexMap<K, T>, mut us: IndexMap<K, U>, ft: FT, fu: FU, ftu: FTU) -> Result<IndexMap<K, V>, E>
+pub fn align<K, T, U, V, FT, FU, FTU>(ts: IndexMap<K, T>, mut us: IndexMap<K, U>, ft: FT, fu: FU, ftu: FTU) -> IndexMap<K, V>
 where
     K: Hash + Eq,
-    FT: Fn(T) -> Result<V, E>,
-    FU: Fn(U) -> Result<V, E>,
-    FTU: Fn(T, U) -> Result<V, E>,
+    FT: Fn(T) -> V,
+    FU: Fn(U) -> V,
+    FTU: Fn(T, U) -> V,
 {
     let mut result: IndexMap<K, V> = IndexMap::new();
 
     for (k, t) in ts {
         match us.swap_remove(&k) {
-            None => result.insert(k, ft(t)?),
-            Some(u) => result.insert(k, ftu(t, u)?),
+            None => result.insert(k, ft(t)),
+            Some(u) => result.insert(k, ftu(t, u)),
         };
     }
 
     for (k, u) in us {
-        result.insert(k, fu(u)?);
+        result.insert(k, fu(u));
     }
-    Ok(result)
+    result
 }
