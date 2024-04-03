39eab0bb32fb02144d5e5f39626a099ea0ca6297
Jesse Hallett <jesse.hallett@hasura.io>
2024-04-02 19:18:01 -0700

    all Any in roundtrip bson-to-json tests

--- crates/mongodb-agent-common/src/query/serialization/tests.rs
+++ crates/mongodb-agent-common/src/query/serialization/tests.rs
@@ -3,18 +3,13 @@ use mongodb::bson::Bson;
 use mongodb_cli_plugin::type_from_bson;
 use mongodb_support::BsonScalarType;
 use proptest::prelude::*;
-use test_helpers::arb_bson::{arb_bson_with_options, arb_datetime, ArbBsonOptions};
+use test_helpers::arb_bson::{arb_bson, arb_datetime};
 
 use super::{bson_to_json, json_to_bson};
 
-// bson_to_json should round-trip with json_to_bson - but note that round-trips do not work for
-// values where the inferred type includes `Any` because in those cases we lose the necessary type
-// information to convert back to BSON losslessly. `Any` appears in an inferred type when a value
-// includes an array with elements of different types. So we limit tests to arrays with uniform
-// types.
 proptest! {
     #[test]
-    fn converts_bson_to_json_and_back(bson in arb_bson_with_options(ArbBsonOptions { heterogeneous_arrays: false, ..Default::default() })) {
+    fn converts_bson_to_json_and_back(bson in arb_bson()) {
         let (object_types, inferred_type) = type_from_bson("test_object", &bson);
         let json = bson_to_json(&inferred_type, &object_types, bson.clone())?;
         let actual = json_to_bson(&inferred_type, &object_types, json)?;
