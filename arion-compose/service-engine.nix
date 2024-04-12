9b43d58b95dfb2cc440e613b76932bcfa3ff5a6b
Jesse Hallett <jesse.hallett@hasura.io>
2024-04-11 23:06:07 -0400

    update local services and fixtures to use latest engine version (#33)
    Update local docker-compose configuration to use the latest engine version. This also switches to pulling engine source from the open source repo.
    
    The implementation for dev-auth-webhook changed to a Rust project, so the nix configuration is updated accordingly.
    
    Ticket: https://hasurahq.atlassian.net/browse/MDB-109

--- arion-compose/service-engine.nix
+++ arion-compose/service-engine.nix
@@ -28,7 +28,7 @@ let
       done
       cat "$combined" \
         | yq -o=json \
-        | jq -s 'map(if .kind == "DataConnectorLink" then .definition.url = { singleUrl: { value: "${connector-url}" } } else . end)' \
+        | jq -s 'map(if .kind == "DataConnectorLink" then .definition.url = { singleUrl: { value: "${connector-url}" } } else . end) | map(select(type != "null"))' \
         > metadata.json
     '';
 
@@ -54,7 +54,7 @@ in
   image.contents = with pkgs.pkgsCross.linux; [
     cacert
     curl
-    v3-engine # added to pkgs via an overlay in flake.nix.
+    graphql-engine # added to pkgs via an overlay in flake.nix.
   ];
   service = withOverrides service {
     useHostStore = true;
