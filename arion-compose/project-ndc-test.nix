93e0b0908da108d7f06198903f0784710095a3e4
Daniel Chambers <daniel@hasura.io>
2024-04-18 09:59:38 +1000

    ndc-test nix improvements and Chinook data fixes (#43)

--- arion-compose/project-ndc-test.nix
+++ arion-compose/project-ndc-test.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 let
   mongodb-port = "27017";
@@ -9,10 +9,21 @@ in
   services = {
     test = import ./service-connector.nix {
       inherit pkgs;
-      command = "test";
+      command = ["test"];
+      # Record snapshots into the snapshots dir
+      # command = ["test" "--snapshots-dir" "/snapshots" "--seed" "1337_1337_1337_1337_1337_1337_13"];
+      # Replay and test the recorded snapshots
+      # command = ["replay" "--snapshots-dir" "/snapshots"];
       configuration-dir = ../fixtures/connector/chinook;
       database-uri = "mongodb://mongodb:${mongodb-port}/chinook";
       service.depends_on.mongodb.condition = "service_healthy";
+      # Run the container as the current user so when it writes to the snapshots directory it doesn't write as root
+      service.user = builtins.toString config.host.uid;
+      extra-volumes = [
+        # Mount the snapshots directory in the repo source tree into the container
+        # so that ndc-test can read/write in it
+        "./snapshots:/snapshots:rw"
+      ];
     };
 
     mongodb = import ./service-mongodb.nix {
--- fixtures/mongodb/chinook/chinook-import.sh
+++ fixtures/mongodb/chinook/chinook-import.sh
@@ -4,41 +4,34 @@ set -euo pipefail
 
 # Get the directory of this script file
 FIXTURES=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DATABASE_NAME=chinook
 
-echo "📡 Importing Chinook..."
+echo "📡 Importing Chinook into database $DATABASE_NAME..."
 
-loadSchema() {
-    local collection="$1"
-    local schema_file="$2"
-    echo "🔐 Applying validation for ${collection}..."
+importCollection() {
+  local collection="$1"
+  local schema_file="$FIXTURES/$collection.schema.json"
+  local data_file="$FIXTURES/$collection.data.json"
+  echo "🔐 Applying validation for ${collection}..."
     mongosh --eval "
         var schema = $(cat "${schema_file}");
         db.createCollection('${collection}', { validator: schema });
-    " chinook
+    " "$DATABASE_NAME"
+
+  echo "⬇️ Importing data for ${collection}..."
+  mongoimport --db "$DATABASE_NAME" --collection "$collection" --type json --jsonArray --file "$data_file"
 }
 
-loadSchema "Album" "$FIXTURES/Album.json"
-loadSchema "Artist" "$FIXTURES/Artist.json"
-loadSchema "Customer" "$FIXTURES/Customer.json"
-loadSchema "Employee" "$FIXTURES/Employee.json"
-loadSchema "Genre" "$FIXTURES/Genre.json"
-loadSchema "Invoice" "$FIXTURES/Invoice.json"
-loadSchema "InvoiceLine" "$FIXTURES/InvoiceLine.json"
-loadSchema "MediaType" "$FIXTURES/MediaType.json"
-loadSchema "Playlist" "$FIXTURES/Playlist.json"
-loadSchema "PlaylistTrack" "$FIXTURES/PlaylistTrack.json"
-loadSchema "Track" "$FIXTURES/Track.json"
-
-mongoimport --db chinook --collection Album --type csv --headerline --file "$FIXTURES"/Album.csv
-mongoimport --db chinook --collection Artist --type csv --headerline --file "$FIXTURES"/Artist.csv
-mongoimport --db chinook --collection Customer --type csv --headerline --file "$FIXTURES"/Customer.csv
-mongoimport --db chinook --collection Employee --type csv --headerline --file "$FIXTURES"/Employee.csv
-mongoimport --db chinook --collection Genre --type csv --headerline --file "$FIXTURES"/Genre.csv
-mongoimport --db chinook --collection Invoice --type csv --headerline --file "$FIXTURES"/Invoice.csv
-mongoimport --db chinook --collection InvoiceLine --type csv --headerline --file "$FIXTURES"/InvoiceLine.csv
-mongoimport --db chinook --collection MediaType --type csv --headerline --file "$FIXTURES"/MediaType.csv
-mongoimport --db chinook --collection Playlist --type csv --headerline --file "$FIXTURES"/Playlist.csv
-mongoimport --db chinook --collection PlaylistTrack --type csv --headerline --file "$FIXTURES"/PlaylistTrack.csv
-mongoimport --db chinook --collection Track --type csv --headerline --file "$FIXTURES"/Track.csv
+importCollection "Album"
+importCollection "Artist"
+importCollection "Customer"
+importCollection "Employee"
+importCollection "Genre"
+importCollection "Invoice"
+importCollection "InvoiceLine"
+importCollection "MediaType"
+importCollection "Playlist"
+importCollection "PlaylistTrack"
+importCollection "Track"
 
 echo "✅ Sample Chinook data imported..."
