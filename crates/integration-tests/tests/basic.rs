469c9217c664f84101c867402224c1efb895bbee
Jesse Hallett <jesse.hallett@hasura.io>
2024-04-19 17:34:34 -0700

    get graphql queries into snapshot files

--- crates/integration-tests/tests/basic.rs
+++ crates/integration-tests/tests/basic.rs
@@ -3,18 +3,22 @@ use integration_tests::query;
 
 #[tokio::test]
 async fn runs_a_query() -> anyhow::Result<()> {
-    let q = r#"
-        query Movies {
-          movies(limit: 10, order_by: { id: Asc }) {
-            title
-            imdb {
-              rating
-              votes
-            }
-          }
-        }
-    "#;
-    let response = query(q).run().await?;
-    assert_yaml_snapshot!(response);
+    assert_yaml_snapshot!(
+        query(
+            r#"
+                query Movies {
+                  movies(limit: 10, order_by: { id: Asc }) {
+                    title
+                    imdb {
+                      rating
+                      votes
+                    }
+                  }
+                }
+            "#
+        )
+        .run()
+        .await?
+    );
     Ok(())
 }
--- crates/integration-tests/tests/remote_relationship.rs
+++ crates/integration-tests/tests/remote_relationship.rs
@@ -4,22 +4,24 @@ use serde_json::json;
 
 #[tokio::test]
 async fn provides_source_and_target_for_remote_relationship() -> anyhow::Result<()> {
-    let q = r#"
-        query AlbumMovies($limit: Int, $movies_limit: Int) {
-          album(limit: $limit, order_by: { title: Asc }) {
-            title
-            movies(limit: $movies_limit) {
-              title
-              runtime
-            }
-            albumId
-          }
-        }
-    "#;
-    let response = query(q)
+    assert_yaml_snapshot!(
+        query(
+            r#"
+                query AlbumMovies($limit: Int, $movies_limit: Int) {
+                  album(limit: $limit, order_by: { title: Asc }) {
+                    title
+                    movies(limit: $movies_limit) {
+                      title
+                      runtime
+                    }
+                    albumId
+                  }
+                }
+            "#
+        )
         .variables(json!({ "limit": 11, "movies_limit": 2 }))
         .run()
-        .await?;
-    assert_yaml_snapshot!(response);
+        .await?
+    );
     Ok(())
 }
