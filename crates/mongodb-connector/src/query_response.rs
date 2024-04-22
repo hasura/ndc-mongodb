ce0d66ad8f398382f65428b9b9f6154a0691305c
Jesse Hallett <jesse.hallett@hasura.io>
2024-04-22 13:58:23 -0700

    distinguish between aggregate and non-aggregate responses

--- crates/mongodb-connector/src/query_response.rs
+++ crates/mongodb-connector/src/query_response.rs
@@ -1,10 +1,13 @@
+use std::collections::BTreeMap;
+
 use anyhow::anyhow;
 use configuration::Configuration;
+use indexmap::IndexMap;
 use itertools::Itertools as _;
-use mongodb::bson;
+use mongodb::bson::{self, from_bson, Bson};
 use ndc_sdk::{
     connector::QueryError,
-    models::{QueryRequest, QueryResponse, RowSet},
+    models::{Query, QueryRequest, QueryResponse, RowFieldValue, RowSet},
 };
 use serde::Deserialize;
 
@@ -15,20 +18,13 @@ struct ResponsesForVariableSets {
     row_sets: Vec<Vec<bson::Document>>,
 }
 
-// #[derive(Debug, Deserialize)]
-// struct ResponseForAVariableSet {
-//     query: BsonRowSet,
-// }
-//
-// #[derive(Debug, Deserialize)]
-// struct SingleResponse {
-//     query: BsonRowSet,
-// }
-
-// #[derive(Debug, Deserialize)]
-// struct BsonRowSet {
-//     rows: Vec<bson::Document>,
-// }
+#[derive(Debug, Deserialize)]
+struct BsonRowSet {
+    #[serde(default)]
+    aggregates: BTreeMap<String, Bson>,
+    #[serde(default)]
+    rows: Vec<bson::Document>,
+}
 
 pub fn serialize_query_response(
     config: &Configuration,
@@ -43,27 +39,66 @@ pub fn serialize_query_response(
         responses
             .row_sets
             .into_iter()
-            .map(|docs| serialize_row_set(docs))
+            .map(|docs| serialize_row_set(&query_request.query, docs))
             .try_collect()
     } else {
         // TODO: in an aggregation response we expect one document instead of a list of documents
-        Ok(vec![serialize_row_set(response_documents)?])
+        Ok(vec![serialize_row_set(
+            &query_request.query,
+            response_documents,
+        )?])
     }?;
     let response = QueryResponse(row_sets);
     tracing::debug!(query_response = %serde_json::to_string(&response).unwrap());
     Ok(response)
 }
 
-fn serialize_row_set(docs: Vec<bson::Document>) -> Result<RowSet, QueryError> {
-    let rows = docs
-        .into_iter()
+fn serialize_row_set(query: &Query, docs: Vec<bson::Document>) -> Result<RowSet, QueryError> {
+    if query
+        .aggregates
+        .as_ref()
+        .unwrap_or(&IndexMap::new())
+        .is_empty()
+    {
+        // When there are no aggregates we expect a list of rows
+        let rows = serialize_rows(docs)?;
+        Ok(RowSet {
+            aggregates: None,
+            rows: Some(rows),
+        })
+    } else {
+        // When there are aggregates we expect a single document with `rows` and `aggregates`
+        // fields
+        let row_set: BsonRowSet = parse_single_document(docs)?;
+        let aggregates: IndexMap<String, serde_json::Value> = row_set
+            .aggregates
+            .into_iter()
+            .map(|(key, value)| {
+                Ok((
+                    key,
+                    from_bson(value).map_err(|err| QueryError::Other(err.into()))?,
+                ))
+            })
+            .try_collect::<_, _, QueryError>()?;
+        let rows = serialize_rows(row_set.rows)?;
+        Ok(RowSet {
+            aggregates: if aggregates.is_empty() {
+                None
+            } else {
+                Some(aggregates)
+            },
+            rows: if rows.is_empty() { None } else { Some(rows) },
+        })
+    }
+}
+
+fn serialize_rows(
+    docs: Vec<bson::Document>,
+) -> Result<Vec<IndexMap<String, RowFieldValue>>, QueryError> {
+    docs.into_iter()
         .map(|doc| bson::from_document(doc))
         .try_collect()
-        .map_err(|err| QueryError::Other(err.into()))?;
-    Ok(RowSet {
-        aggregates: None,
-        rows: Some(rows),
-    })
+        .map_err(|err| QueryError::Other(err.into()))
 }
 
 fn parse_single_document<T>(documents: Vec<bson::Document>) -> Result<T, QueryError>
