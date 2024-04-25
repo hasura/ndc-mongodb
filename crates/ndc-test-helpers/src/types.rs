d2d8dca25ef194bed390d10b5ee6d8615b232d34
Jesse Hallett <jesse.hallett@hasura.io>
2024-04-24 18:44:06 -0700

    delete commented code

--- crates/ndc-test-helpers/src/types.rs
+++ crates/ndc-test-helpers/src/types.rs
@@ -2,28 +2,6 @@ use std::fmt::Display;
 
 use ndc_models::{ObjectField, ObjectType, Type};
 
-// pub fn object_type(
-//     name: impl Display,
-//     fields: impl IntoIterator<Item = (impl ToString, impl Into<Type>)>,
-// ) -> (String, ObjectType) {
-//     let t = ObjectType {
-//         description: Default::default(),
-//         fields: fields
-//             .into_iter()
-//             .map(|(name, field_type)| {
-//                 (
-//                     name.to_string(),
-//                     ObjectField {
-//                         description: Default::default(),
-//                         r#type: field_type.into(),
-//                     },
-//                 )
-//             })
-//             .collect(),
-//     };
-//     (name.to_string(), t)
-// }
-
 pub fn object_type(
     fields: impl IntoIterator<Item = (impl ToString, impl Into<Type>)>,
 ) -> ObjectType {
