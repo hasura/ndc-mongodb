ea3cba7806a8d50d6c439739007f37bc3e31445c
Brandon Martin <brandon@codedmart.com>
2024-04-26 18:48:38 -0600

    Add tracing spans and update SDK (#58)

--- crates/mongodb-agent-common/src/query/execute_query_request.rs
+++ crates/mongodb-agent-common/src/query/execute_query_request.rs
@@ -3,6 +3,7 @@ use dc_api_types::QueryRequest;
 use futures::Stream;
 use futures_util::TryStreamExt as _;
 use mongodb::bson;
+use tracing::Instrument;
 
 use super::pipeline::pipeline_for_query_request;
 use crate::{
@@ -21,24 +22,39 @@ pub async fn execute_query_request(
     query_request: QueryRequest,
 ) -> Result<Vec<bson::Document>, MongoAgentError> {
     let target = QueryTarget::for_request(config, &query_request);
-    let pipeline = pipeline_for_query_request(config, &query_request)?;
+    let pipeline = tracing::info_span!("Build Query Pipeline").in_scope(|| {
+        pipeline_for_query_request(config, &query_request)
+    })?;
     tracing::debug!(
         ?query_request,
         ?target,
         pipeline = %serde_json::to_string(&pipeline).unwrap(),
         "executing query"
     );
-
     // The target of a query request might be a collection, or it might be a native query. In the
     // latter case there is no collection to perform the aggregation against. So instead of sending
     // the MongoDB API call `db.<collection>.aggregate` we instead call `db.aggregate`.
-    let documents = match target.input_collection() {
-        Some(collection_name) => {
-            let collection = database.collection(collection_name);
-            collect_from_cursor(collection.aggregate(pipeline, None).await?).await
+    let documents = async move {
+        match target.input_collection() {
+            Some(collection_name) => {
+                let collection = database.collection(collection_name);
+                collect_from_cursor(
+                        collection.aggregate(pipeline, None)
+                        .instrument(tracing::info_span!("Process Pipeline", internal.visibility = "user"))
+                        .await?
+                    )
+                    .await
+            }
+            None => collect_from_cursor(
+                    database.aggregate(pipeline, None)
+                    .instrument(tracing::info_span!("Process Pipeline", internal.visibility = "user"))
+                    .await?
+                )
+                .await,
         }
-        None => collect_from_cursor(database.aggregate(pipeline, None).await?).await,
-    }?;
+    }
+    .instrument(tracing::info_span!("Execute Query Pipeline", internal.visibility = "user"))
+    .await?;
     tracing::debug!(response_documents = %serde_json::to_string(&documents).unwrap(), "response from MongoDB");
 
     Ok(documents)
@@ -51,5 +67,6 @@ async fn collect_from_cursor(
         .into_stream()
         .map_err(MongoAgentError::MongoDB)
         .try_collect::<Vec<_>>()
+        .instrument(tracing::info_span!("Collect Pipeline", internal.visibility = "user"))
         .await
 }
