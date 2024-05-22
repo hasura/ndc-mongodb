8bc39bff1b931f1daa43c81e5c957140ee981046
Jesse Hallett <jesse.hallett@hasura.io>
2024-05-22 10:37:09 -0700

    simplify QueryPlanState - assume engine-provided relations do not need duplication

--- crates/ndc-query-plan/src/plan_for_query_request/query_plan_state.rs
+++ crates/ndc-query-plan/src/plan_for_query_request/query_plan_state.rs
@@ -1,7 +1,6 @@
 use std::{
     cell::{Cell, RefCell},
     collections::BTreeMap,
-    ops::Deref,
     rc::Rc,
 };
 
@@ -51,7 +50,7 @@ impl<T: QueryContext> QueryPlanState<'_, T> {
     /// When traversing a query request into a sub-query we enter a new scope for relationships.
     /// Use this function to get a new plan for the new scope. Shares query-request-level state
     /// with the parent plan.
-    pub fn state_for_subquery<'a>(&'a self) -> QueryPlanState<'a, T> {
+    pub fn state_for_subquery(&self) -> QueryPlanState<'_, T> {
         QueryPlanState {
             context: self.context,
             collection_relationships: self.collection_relationships,
@@ -63,59 +62,42 @@ impl<T: QueryContext> QueryPlanState<'_, T> {
 
     /// Record a relationship reference so that it is added to the list of joins for the query
     /// plan, and get back an identifier than can be used to access the joined collection.
-    pub fn register_relationship<'a>(
-        &'a mut self,
+    pub fn register_relationship(
+        &mut self,
         ndc_relationship_name: String,
         arguments: BTreeMap<String, RelationshipArgument>,
         query: Query<T>,
-    ) -> Result<(&'a str, &'a Relationship<T>)> {
-        let ndc_relationship =
-            lookup_relationship(self.collection_relationships, &ndc_relationship_name)?;
+    ) -> Result<(&str, &Relationship<T>)> {
+        let already_registered = self.relationships.contains_key(&ndc_relationship_name);
 
-        let relationship = Relationship {
-            column_mapping: ndc_relationship.column_mapping.clone(),
-            relationship_type: ndc_relationship.relationship_type,
-            target_collection: ndc_relationship.target_collection.clone(),
-            arguments,
-            query,
-        };
+        if !already_registered {
+            let ndc_relationship =
+                lookup_relationship(self.collection_relationships, &ndc_relationship_name)?;
 
-        // We want to insert the relationship into the internal map if there isn't already
-        // a matching relationship in there, then return references to the key and to the
-        // relationship. Lifetime analysis makes this fiddly. We get the key (creating one if
-        // necessary), and then do the lookup.
+            let relationship = Relationship {
+                column_mapping: ndc_relationship.column_mapping.clone(),
+                relationship_type: ndc_relationship.relationship_type,
+                target_collection: ndc_relationship.target_collection.clone(),
+                arguments,
+                query,
+            };
 
-        let key = match self.find_matching_relationship_key(&relationship) {
-            Some(key) => key,
-            None => {
-                let key = self.unique_name(ndc_relationship_name);
-                self.relationships.insert(key.clone(), relationship);
-                key
-            }
-        };
+            self.relationships
+                .insert(ndc_relationship_name.clone(), relationship);
+        }
 
-        // Safety: we just inserted this key if it wasn't already present
-        let (key, relationship) = self.relationships.get_key_value(&key).unwrap();
+        // Safety: we just inserted this key
+        let (key, relationship) = self
+            .relationships
+            .get_key_value(&ndc_relationship_name)
+            .unwrap();
         Ok((key, relationship))
-    }
-
-    fn find_matching_relationship_key<'a>(
-        &'a self,
-        relationship: &Relationship<T>,
-    ) -> Option<String> {
-        self.relationships.iter().find_map(|(key, rel)| {
-            if rel == relationship {
-                Some(key.clone())
-            } else {
-                None
-            }
-        })
     }
 
     /// Record a collection reference so that it is added to the list of joins for the query
     /// plan, and get back an identifier than can be used to access the joined collection.
-    pub fn register_unrelated_join<'a>(
-        &'a mut self,
+    pub fn register_unrelated_join(
+        &mut self,
         target_collection: String,
         arguments: BTreeMap<String, RelationshipArgument>,
         query: Query<T>,
@@ -126,19 +108,8 @@ impl<T: QueryContext> QueryPlanState<'_, T> {
             query,
         };
 
-        let matching_key = {
-            let unrelated_joins = RefCell::borrow(&self.unrelated_joins);
-            Self::find_matching_join_key(unrelated_joins, &join)
-        };
-
-        let key = match matching_key {
-            Some(key) => key,
-            None => {
-                let key = self.unique_name(format!("__join_{}", join.target_collection));
-                self.unrelated_joins.borrow_mut().insert(key.clone(), join);
-                key
-            }
-        };
+        let key = self.unique_name(format!("__join_{}", join.target_collection));
+        self.unrelated_joins.borrow_mut().insert(key.clone(), join);
 
         // Unlike [Self::register_relationship] this method does not return a reference to the
         // registered join. If we need that reference then we need another [RefCell::borrow] call
@@ -147,21 +118,6 @@ impl<T: QueryContext> QueryPlanState<'_, T> {
         // still alive the next time [Self::register_unrelated_join] is called then the borrow_mut
         // call will fail.
         key
-    }
-
-    fn find_matching_join_key<'a>(
-        registered_joins: impl Deref<Target = BTreeMap<String, UnrelatedJoin<T>>>,
-        join: &UnrelatedJoin<T>,
-    ) -> Option<String> {
-        registered_joins.iter().find_map(
-            |(key, jn)| {
-                if jn == join {
-                    Some(key.clone())
-                } else {
-                    None
-                }
-            },
-        )
     }
 
     /// Use this for subquery plans to get the relationships for each sub-query
@@ -174,21 +130,9 @@ impl<T: QueryContext> QueryPlanState<'_, T> {
         self.unrelated_joins.take()
     }
 
-    // pub fn into_join_plan(self) -> JoinPlan<T> {
-    //     JoinPlan {
-    //         relationships: self.relationships.into_iter().collect(),
-    //         unrelated_joins: self.unrelated_joins.take().into_iter().collect(),
-    //     }
-    // }
-
     fn unique_name(&mut self, name: String) -> String {
         let count = self.counter.get();
         self.counter.set(count + 1);
         format!("{name}_{count}")
     }
 }
-
-// pub struct JoinPlan<T: ConnectorTypes> {
-//     pub relationships: BTreeMap<String, Relationship<T>>,
-//     pub unrelated_joins: BTreeMap<String, UnrelatedJoin<T>>,
-// }
