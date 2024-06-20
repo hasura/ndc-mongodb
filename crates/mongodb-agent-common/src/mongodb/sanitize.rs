55c04645943604cdf0c66bc15db25f08ee255b33
Jesse Hallett <jesse.hallett@hasura.io>
2024-06-20 15:38:25 -0700

    test variable name escaping function

--- crates/mongodb-agent-common/src/mongodb/sanitize.rs
+++ crates/mongodb-agent-common/src/mongodb/sanitize.rs
@@ -52,3 +52,92 @@ pub fn safe_name(name: &str) -> Result<Cow<str>, MongoAgentError> {
         Ok(Cow::Borrowed(name))
     }
 }
+
+// The escape character must be a valid character in MongoDB variable names, but must not appear in
+// lower-case hex strings. A non-ASCII character works if we specifically map it to a two-character
+// hex escape sequence (see [ESCAPE_CHAR_ESCAPE_SEQUENCE]). Another option would be to use an
+// allowed ASCII character such as 'x'.
+const ESCAPE_CHAR: char = '·';
+
+/// We want all escape sequences to be two-character hex strings so this must be a value that does
+/// not represent an ASCII character, and that is <= 0xff.
+const ESCAPE_CHAR_ESCAPE_SEQUENCE: u32 = 0xff;
+
+/// MongoDB variable names allow a limited set of ASCII characters, or any non-ASCII character.
+/// See https://www.mongodb.com/docs/manual/reference/aggregation-variables/
+pub fn escape_invalid_variable_chars(input: &str) -> String {
+    let mut encoded = String::new();
+    for char in input.chars() {
+        match char {
+            ESCAPE_CHAR => push_encoded_char(&mut encoded, ESCAPE_CHAR_ESCAPE_SEQUENCE),
+            'a'..='z' | 'A'..='Z' | '0'..='9' | '_' => encoded.push(char),
+            char if char as u32 <= 127 => push_encoded_char(&mut encoded, char as u32),
+            char => encoded.push(char),
+        }
+    }
+    encoded
+}
+
+/// Escape invalid characters using the escape character followed by a two-character hex sequence
+/// that gives the character's ASCII codepoint
+fn push_encoded_char(encoded: &mut String, char: u32) {
+    encoded.push(ESCAPE_CHAR);
+    let zero_pad = if char < 0x10 { "0" } else { "" };
+    encoded.push_str(&format!("{zero_pad}{char:x}"));
+}
+
+#[cfg(test)]
+mod tests {
+    use proptest::prelude::*;
+
+    use super::{escape_invalid_variable_chars, ESCAPE_CHAR, ESCAPE_CHAR_ESCAPE_SEQUENCE};
+
+    proptest! {
+        // Escaped strings must be consistent and distinct. A round-trip test demonstrates this.
+        #[test]
+        fn escaping_variable_chars_roundtrips(input in any::<String>()) {
+            let encoded = escape_invalid_variable_chars(&input);
+            let decoded = unescape_invalid_variable_chars(&encoded);
+            prop_assert_eq!(decoded, input, "encoded string: {}", encoded)
+        }
+    }
+
+    proptest! {
+        // Escaped strings must be consistent and distinct. A round-trip test demonstrates this.
+        #[test]
+        fn escaped_variable_names_are_valid(input in any::<String>()) {
+            let encoded = escape_invalid_variable_chars(&input);
+            prop_assert!(
+                encoded.chars().all(|char|
+                    char as u32 > 127 ||
+                        ('a'..='z').contains(&char) ||
+                        ('A'..='Z').contains(&char) ||
+                        ('0'..='9').contains(&char) ||
+                        char == '_'
+                ),
+                "encoded string contains only valid characters\nencoded string: {}",
+                encoded
+            )
+        }
+    }
+
+    fn unescape_invalid_variable_chars(input: &str) -> String {
+        let mut decoded = String::new();
+        let mut chars = input.chars();
+        while let Some(char) = chars.next() {
+            if char == ESCAPE_CHAR {
+                let escape_sequence = [chars.next().unwrap(), chars.next().unwrap()];
+                let code_point =
+                    u32::from_str_radix(&escape_sequence.iter().collect::<String>(), 16).unwrap();
+                if code_point == ESCAPE_CHAR_ESCAPE_SEQUENCE {
+                    decoded.push(ESCAPE_CHAR)
+                } else {
+                    decoded.push(char::from_u32(code_point).unwrap())
+                }
+            } else {
+                decoded.push(char)
+            }
+        }
+        decoded
+    }
+}
--- crates/mongodb-agent-common/src/query/query_variable_name.rs
+++ crates/mongodb-agent-common/src/query/query_variable_name.rs
@@ -2,7 +2,10 @@ use std::borrow::Cow;
 
 use configuration::MongoScalarType;
 
-use crate::mongo_query_plan::{ObjectType, Type};
+use crate::{
+    mongo_query_plan::{ObjectType, Type},
+    mongodb::sanitize::escape_invalid_variable_chars,
+};
 
 /// Maps a variable name and type from a [ndc_models::QueryRequest] `variables` map to a variable
 /// name for use in a MongoDB aggregation pipeline. The type is incorporated into the produced name
@@ -16,7 +19,7 @@ use crate::mongo_query_plan::{ObjectType, Type};
 /// - It must produce a valid MongoDB variable name (see https://www.mongodb.com/docs/manual/reference/aggregation-variables/)
 pub fn query_variable_name(name: &str, variable_type: &Type) -> String {
     let output = format!("var_{name}_{}", type_name(variable_type));
-    escape_invalid_ascii(&output)
+    escape_invalid_variable_chars(&output)
 }
 
 fn type_name(input_type: &Type) -> Cow<'static, str> {
@@ -38,32 +41,11 @@ fn object_type_name(obj: &ObjectType) -> String {
     output
 }
 
-const ESCAPE_CHAR: char = '·';
-
-/// MongoDB variable names allow a limited set of ASCII characters, or any non-ASCII character.
-/// See https://www.mongodb.com/docs/manual/reference/aggregation-variables/
-fn escape_invalid_ascii(input: &str) -> String {
-    let mut encoded = String::new();
-    for char in input.chars() {
-        match char {
-            'a'..='z' | 'A'..='Z' | '0'..='9' | '_' => encoded.push(char),
-            ESCAPE_CHAR => push_encoded_char(&mut encoded, char),
-            char if char as u32 <= 127 => push_encoded_char(&mut encoded, char),
-            char => encoded.push(char),
-        }
-    }
-    encoded
-}
-
-/// Escape invalid characters using the escape character followed by the character's Unicode
-/// codepoint in lowercase hex format
-fn push_encoded_char(encoded: &mut String, char: char) {
-    encoded.push(ESCAPE_CHAR);
-    encoded.push_str(&format!("{:x}", char as u32));
-}
-
 #[cfg(test)]
 mod tests {
+    use proptest::prelude::*;
+    use test_helpers::arb_bson;
+
     proptest! {
         #[test]
         fn variable_names_are_reproducible(bson in arb_bson()) {
