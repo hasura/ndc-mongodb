ae1df8092b591b9ef1b414f3fc8c90c253c84899
Jesse Hallett <jesse.hallett@hasura.io>
2024-07-03 18:02:05 -0700

    wip:

--- crates/integration-tests/src/tests/basic.rs
+++ crates/integration-tests/src/tests/basic.rs
@@ -1,5 +1,6 @@
 use crate::graphql_query;
 use insta::assert_yaml_snapshot;
+use serde_json::json;
 
 #[tokio::test]
 async fn runs_a_query() -> anyhow::Result<()> {
@@ -17,6 +18,30 @@ async fn runs_a_query() -> anyhow::Result<()> {
                 }
             "#
         )
+        .run()
+        .await?
+    );
+    Ok(())
+}
+
+#[tokio::test]
+async fn sorts_string_column_value_by_date() -> anyhow::Result<()> {
+    assert_yaml_snapshot!(
+        graphql_query(
+            r#"
+                query UpdatedAfter($dateInput: Date) {
+                  movies(
+                    limit: 10,
+                    order_by: {id: Asc},
+                    where: {lastupdated: {_gt: $dateInput}}
+                  ) {
+                    title
+                    lastupdated
+                  }
+                }
+            "#
+        )
+        .variables(json!({ "dateInput": "2016-01-01" }))
         .run()
         .await?
     );
--- crates/integration-tests/src/tests/remote_relationship.rs
+++ crates/integration-tests/src/tests/remote_relationship.rs
@@ -1,6 +1,6 @@
 use crate::{graphql_query, run_connector_query};
 use insta::assert_yaml_snapshot;
-use ndc_test_helpers::{binop, field, query, query_request, target, variable};
+use ndc_test_helpers::{asc, binop, field, query, query_request, target, variable};
 use serde_json::json;
 
 #[tokio::test]
@@ -66,3 +66,34 @@ async fn handles_request_with_single_variable_set() -> anyhow::Result<()> {
     );
     Ok(())
 }
+
+// #[tokio::test]
+// async fn variable_type_does_not_match_column_type() -> anyhow::Result<()> {
+//     // Skip this test in MongoDB 5 because the example fails there. We're getting an error:
+//     //
+//     // > Kind: Command failed: Error code 5491300 (Location5491300): $documents' is not allowed in user requests, labels: {}
+//     //
+//     // This means that remote joins are not working in MongoDB 5
+//     if let Ok(image) = std::env::var("MONGODB_IMAGE") {
+//         if image == "mongo:5" {
+//             return Ok(());
+//         }
+//     }
+//
+//     assert_yaml_snapshot!(
+//         run_connector_query(
+//             query_request()
+//                 .variables([[("count", 1)], [("count", 2)], [("count", 3)]])
+//                 .collection("title_word_frequency")
+//                 .query(
+//                     query()
+//                         .predicate(binop("_eq", target!("count"), variable!(count)))
+//                         .order_by([asc!("_id")])
+//                         .limit(20)
+//                         .fields([field!("_id"), field!("count")]),
+//                 )
+//         )
+//         .await?
+//     );
+//     Ok(())
+// }
