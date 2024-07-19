0970d4f803b5d0277d6eb15de254ed725f7e5300
Jesse Hallett <jesse.hallett@hasura.io>
2024-07-19 00:40:51 -0700

    update connector to use MutationPlan

--- crates/mongodb-agent-common/src/procedure/mod.rs
+++ crates/mongodb-agent-common/src/procedure/mod.rs
@@ -5,12 +5,15 @@ use std::borrow::Cow;
 use std::collections::BTreeMap;
 
 use configuration::native_mutation::NativeMutation;
+use itertools::Itertools as _;
+use mongodb::bson::Bson;
 use mongodb::options::SelectionCriteria;
 use mongodb::{bson, Database};
-use ndc_models::Argument;
+use ndc_models::ArgumentName;
 
-use crate::mongo_query_plan::Type;
-use crate::query::arguments::resolve_arguments;
+use crate::mongo_query_plan::{MutationProcedureArgument, Type};
+use crate::query::make_selector;
+use crate::query::serialization::json_to_bson;
 
 pub use self::error::ProcedureError;
 pub use self::interpolated_command::interpolated_command;
@@ -18,9 +21,8 @@ pub use self::interpolated_command::interpolated_command;
 /// Encapsulates running arbitrary mongodb commands with interpolated arguments
 #[derive(Clone, Debug)]
 pub struct Procedure<'a> {
-    arguments: BTreeMap<ndc_models::ArgumentName, serde_json::Value>,
+    arguments: BTreeMap<ndc_models::ArgumentName, MutationProcedureArgument>,
     command: Cow<'a, bson::Document>,
-    parameters: Cow<'a, BTreeMap<ndc_models::ArgumentName, Type>>,
     result_type: Type,
     selection_criteria: Option<Cow<'a, SelectionCriteria>>,
 }
@@ -28,12 +30,11 @@ pub struct Procedure<'a> {
 impl<'a> Procedure<'a> {
     pub fn from_native_mutation(
         native_mutation: &'a NativeMutation,
-        arguments: BTreeMap<ndc_models::ArgumentName, serde_json::Value>,
+        arguments: BTreeMap<ndc_models::ArgumentName, MutationProcedureArgument>,
     ) -> Self {
         Procedure {
             arguments,
             command: Cow::Borrowed(&native_mutation.command),
-            parameters: Cow::Borrowed(&native_mutation.arguments),
             result_type: native_mutation.result_type.clone(),
             selection_criteria: native_mutation
                 .selection_criteria
@@ -47,25 +48,50 @@ impl<'a> Procedure<'a> {
         database: Database,
     ) -> Result<(bson::Document, Type), ProcedureError> {
         let selection_criteria = self.selection_criteria.map(Cow::into_owned);
-        let command = interpolate(&self.parameters, self.arguments, &self.command)?;
+        let command = interpolate(self.arguments, &self.command)?;
         let result = database.run_command(command, selection_criteria).await?;
         Ok((result, self.result_type))
     }
 
     pub fn interpolated_command(self) -> Result<bson::Document, ProcedureError> {
-        interpolate(&self.parameters, self.arguments, &self.command)
+        interpolate(self.arguments, &self.command)
     }
 }
 
 fn interpolate(
-    parameters: &BTreeMap<ndc_models::ArgumentName, Type>,
-    arguments: BTreeMap<ndc_models::ArgumentName, serde_json::Value>,
+    arguments: BTreeMap<ndc_models::ArgumentName, MutationProcedureArgument>,
     command: &bson::Document,
 ) -> Result<bson::Document, ProcedureError> {
-    let arguments = arguments
+    let bson_arguments = arguments
         .into_iter()
-        .map(|(name, value)| (name, Argument::Literal { value }))
-        .collect();
-    let bson_arguments = resolve_arguments(parameters, arguments)?;
+        .map(|(name, argument)| {
+            let bson = argument_to_mongodb_expression(&name, argument)?;
+            Ok((name, bson)) as Result<_, ProcedureError>
+        })
+        .try_collect()?;
     interpolated_command(command, &bson_arguments)
+}
+
+fn argument_to_mongodb_expression(
+    name: &ArgumentName,
+    argument: MutationProcedureArgument,
+) -> Result<Bson, ProcedureError> {
+    let bson = match argument {
+        MutationProcedureArgument::Literal {
+            value,
+            argument_type,
+        } => json_to_bson(&argument_type, value).map_err(|error| {
+            ProcedureError::ErrorParsingArgument {
+                argument_name: name.to_string(),
+                error,
+            }
+        })?,
+        MutationProcedureArgument::Predicate { expression } => make_selector(&expression)
+            .map_err(|error| ProcedureError::ErrorParsingPredicate {
+                argument_name: name.to_string(),
+                error: Box::new(error),
+            })?
+            .into(),
+    };
+    Ok(bson)
 }
