745c5697b235ac612d6519390aea055a4c438dde
Jesse Hallett <jesse.hallett@hasura.io>
2024-09-30 12:00:14 -0700

    wip:

---
+++ crates/integration-tests/src/tests/expressions.rs
@@ -0,0 +1,22 @@
+use insta::assert_yaml_snapshot;
+
+use crate::graphql_query;
+
+#[tokio::test]
+async fn evaluates_field_name_that_requires_escaping_in_nested_expression() -> anyhow::Result<()> {
+    assert_yaml_snapshot!(
+        graphql_query(
+            r#"
+                query Filtering {
+                  extendedJsonTestData(where: { value: { _regex: "hello" } }) {
+                    type
+                    value
+                  }
+                }
+            "#
+        )
+        .run()
+        .await?
+    );
+    Ok(())
+}
