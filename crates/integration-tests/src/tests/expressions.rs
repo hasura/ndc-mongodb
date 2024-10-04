b02ce6b58d11b4fb2f8d81fd357e2a7ce5cc4fee
Jesse Hallett <jesse.hallett@hasura.io>
2024-10-04 09:45:00 -0700

    another test needs to be skipped in MongoDB 5

--- crates/integration-tests/src/tests/expressions.rs
+++ crates/integration-tests/src/tests/expressions.rs
@@ -8,6 +8,17 @@ use crate::{connector::Connector, graphql_query, run_connector_query};
 
 #[tokio::test]
 async fn evaluates_field_name_that_requires_escaping_in_nested_expression() -> anyhow::Result<()> {
+    // Skip this test in MongoDB 5 because the example fails there. We're getting an error:
+    //
+    // > Kind: Command failed: Error code 5491300 (Location5491300): $documents' is not allowed in user requests, labels: {}
+    //
+    // This means that remote joins are not working in MongoDB 5
+    if let Ok(image) = std::env::var("MONGODB_IMAGE") {
+        if image == "mongo:5" {
+            return Ok(());
+        }
+    }
+
     assert_yaml_snapshot!(
         graphql_query(
             r#"
