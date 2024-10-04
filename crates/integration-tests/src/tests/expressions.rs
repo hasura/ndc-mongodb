c605197f0b425f7402772813d33f2ca4cf0024a1
Jesse Hallett <jesse.hallett@hasura.io>
2024-10-03 17:37:40 -0700

    add some integration tests

--- crates/integration-tests/src/tests/expressions.rs
+++ crates/integration-tests/src/tests/expressions.rs
@@ -1,8 +1,11 @@
 use insta::assert_yaml_snapshot;
+use ndc_models::ExistsInCollection;
+use ndc_test_helpers::{
+    binop, exists, field, query, query_request, relation_field, relationship, target, value,
+};
 
-use crate::graphql_query;
+use crate::{connector::Connector, graphql_query, run_connector_query};
 
-// TODO: this duplicates a test in basic
 #[tokio::test]
 async fn evaluates_field_name_that_requires_escaping_in_nested_expression() -> anyhow::Result<()> {
     assert_yaml_snapshot!(
@@ -17,6 +20,37 @@ async fn evaluates_field_name_that_requires_escaping_in_nested_expression() -> a
             "#
         )
         .run()
+        .await?
+    );
+    Ok(())
+}
+
+#[tokio::test]
+async fn evaluates_exists_with_predicate() -> anyhow::Result<()> {
+    assert_yaml_snapshot!(
+        run_connector_query(
+            Connector::Chinook,
+            query_request()
+                .collection("Artist")
+                .query(
+                    query()
+                        .predicate(exists(
+                            ExistsInCollection::Related {
+                                relationship: "albums".into(),
+                                arguments: Default::default(),
+                            },
+                            binop("_iregex", target!("Title"), value!("Wild"))
+                        ))
+                        .fields([
+                            field!("_id"),
+                            field!("Name"),
+                            relation_field!("albums" => "albums", query().fields([
+                                field!("Title")
+                            ]))
+                        ]),
+                )
+                .relationships([("albums", relationship("Album", [("ArtistId", "ArtistId")]))])
+        )
         .await?
     );
     Ok(())
