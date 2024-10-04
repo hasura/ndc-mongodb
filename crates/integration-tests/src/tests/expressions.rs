81b9283e457ab8ab877dcba9027d5cca027fbf7c
Jesse Hallett <jesse.hallett@hasura.io>
2024-10-04 10:01:54 -0700

    fix another integration test non-deterministic ordering issue

--- crates/integration-tests/src/tests/expressions.rs
+++ crates/integration-tests/src/tests/expressions.rs
@@ -1,7 +1,7 @@
 use insta::assert_yaml_snapshot;
 use ndc_models::ExistsInCollection;
 use ndc_test_helpers::{
-    binop, exists, field, query, query_request, relation_field, relationship, target, value,
+    asc, binop, exists, field, query, query_request, relation_field, relationship, target, value,
 };
 
 use crate::{connector::Connector, graphql_query, run_connector_query};
@@ -57,7 +57,7 @@ async fn evaluates_exists_with_predicate() -> anyhow::Result<()> {
                             field!("Name"),
                             relation_field!("albums" => "albums", query().fields([
                                 field!("Title")
-                            ]))
+                            ]).order_by([asc!("Title")]))
                         ]),
                 )
                 .relationships([("albums", relationship("Album", [("ArtistId", "ArtistId")]))])
