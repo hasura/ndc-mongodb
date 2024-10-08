2e90f40d0bc66ae81a46ddf632236223b384c7f5
Jesse Hallett <jesse.hallett@hasura.io>
2024-10-08 12:33:44 -0700

    ci: run integration tests with MongoDB v8; remove MongoDB v5 from testing (#112)
    MongoDB v8 has a new stable release so it's past time to include it in our tests.
    
    MongoDB v5 is EOL this month, and it doesn't support our remote join implementation anyway so it's been off our supported versions list for a while. We've been having to skip a bunch of tests in that version, and now we don't have to do that anymore.

--- crates/integration-tests/src/tests/filtering.rs
+++ crates/integration-tests/src/tests/filtering.rs
@@ -5,17 +5,6 @@ use crate::{connector::Connector, graphql_query, run_connector_query};
 
 #[tokio::test]
 async fn filters_on_extended_json_using_string_comparison() -> anyhow::Result<()> {
-    // Skip this test in MongoDB 5 because the example fails there. We're getting an error:
-    //
-    // > Kind: Command failed: Error code 5491300 (Location5491300): $documents' is not allowed in user requests, labels: {}
-    //
-    // This doesn't affect native queries that don't use the $documents stage.
-    if let Ok(image) = std::env::var("MONGODB_IMAGE") {
-        if image == "mongo:5" {
-            return Ok(());
-        }
-    }
-
     assert_yaml_snapshot!(
         graphql_query(
             r#"
@@ -76,17 +65,6 @@ async fn filters_by_comparisons_on_elements_of_array_of_scalars() -> anyhow::Res
 #[tokio::test]
 async fn filters_by_comparisons_on_elements_of_array_of_scalars_against_variable(
 ) -> anyhow::Result<()> {
-    // Skip this test in MongoDB 5 because the example fails there. We're getting an error:
-    //
-    // > Kind: Command failed: Error code 5491300 (Location5491300): $documents' is not allowed in user requests, labels: {}
-    //
-    // This doesn't affect native queries that don't use the $documents stage.
-    if let Ok(image) = std::env::var("MONGODB_IMAGE") {
-        if image == "mongo:5" {
-            return Ok(());
-        }
-    }
-
     assert_yaml_snapshot!(
         run_connector_query(
             Connector::SampleMflix,
